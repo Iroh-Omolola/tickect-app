c0eaff547f001b205265efda902a44ca7a05f400
Iroh-Omolola <irohomolola@gmail.com>
2023-02-27 19:42:13 +0100

    installed jest properties

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,8 @@ import jwt from 'jsonwebtoken';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '@herotickets/common';
+import { BadRequestError } from '@herotickets/common';
 
 const router = express.Router();
 console.log("weeee")
